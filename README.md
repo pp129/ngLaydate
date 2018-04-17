3bd3bfd319cd61aef5b26d2b5a94eb1b775ec253
panfei <604042454@qq.com>
2018-04-17 14:48:24 +0800

    ngModel

--- README.md
+++ README.md
@@ -15,7 +15,8 @@
     <!--时间-->
     <input ng-lay-date type="text" ng-model="date3" range="~" format="HH:mm:ss" />
     <!--时间范围+格式化+双向绑定-->
-    <input ng-lay-date type="text" ng-model="myDate.value" range="{{myDate.range}}" format="{{myDate.format}}">
+	<input ng-lay-date type="text" ng-model="myDate.value" range="{{myDate.range}}" format="{{myDate.format}}">
+	<button ng-click="setRange()">2013-01-29 13:14:11 ~ 2018-04-17 10:51:00</button>
 
     <script src="angular.js"></script>
     <script src="jquery.min.js"></script>
@@ -29,9 +30,17 @@
                     "startDate": "2018-04-03 00:00:00",
                     "endDate": "2018-04-19 00:00:00"
                 },
-                format: 'yyyy-MM-dd HH:mm:ss',
-                range: '到'
-            };
+                format: 'yyyy-MM-dd HH:mm:ss'
+			};
+			$scope.setRange = function () {
+				$scope.myDate = {
+					value: {
+						"startDate": "2013-01-29 13:14:11",
+						"endDate": "2018-04-17 10:51:00"
+					},
+					format: 'yyyy-MM-dd HH:mm:ss'
+				};
+			}
         })
     </script>
 ```
--- ngLayDate.js
+++ ngLayDate.js
@@ -1,68 +1,84 @@
 if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
-    module.exports = 'ngLayDate';
+	module.exports = 'ngLayDate';
 }
 angular.module('ngLayDate', [])
-    .directive('ngLayDate', function ($timeout) {
-        return {
-            require: '?ngModel',
-            restrict: 'A',
-            scope: {
-                ngModel: '='
-            },
-            link: function (scope, element, attr, ngModel) {
-                var _date = null,
-                    _config = {};
-                // 渲染模板完成后执行
-                $timeout(function () {
-                    // 初始化参数
-                    //console.log(attr.range)
-                    _config = {
-                        elem: element[0],
-                        value: ngModel.$modelValue && ngModel.$modelValue.startDate && ngModel.$modelValue.endDate ? ngModel.$modelValue.startDate + ' ' + (attr.range !== undefined && attr.range !== '' ? attr.range : '~') + ' ' + ngModel.$modelValue.endDate : ngModel.$modelValue ? ngModel.$modelValue : '',
-                        format: attr.format !== undefined && attr.format !== '' ? attr.format : 'yyyy-MM-dd',
-                        range: attr.range !== undefined && attr.range !== '' ? attr.range : false,
-                        type: attr.format !== undefined && attr.format !== '' && attr.format === 'yyyy-MM-dd HH:mm:ss' ? 'datetime' : attr.format === 'yyyy' ? 'year' : attr.format === 'yyyy-MM' ? 'month' : attr.format === 'HH:mm:ss' ? 'time' : 'date',
-                        //theme: '#5db2ff',
-                        //max:attr.hasOwnProperty('maxDate')?attr.maxDate:'',
-                        //min:attr.hasOwnProperty('minDate')?attr.minDate:'',
-                        done: function (value, date, endDate) {
-                            //console.log(value); //得到日期生成的值，如：2017-08-18
-                            //console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
-                            //console.log(this.range);
-                            var i = 0;
-                            if (this.range) {
-                                i = value.indexOf(this.range);
-                                ngModel.$setViewValue({
-                                    startDate: value.substring(0, i).trim(),
-                                    endDate: value.substring(i + 1).trim()
-                                });
-                            } else {
-                                ngModel.$setViewValue(value);
-                            }
-                            //console.log(value.substring(0, i).trim());
-                            //console.log(new Date(value.substring(0, i).trim()));
-                            //console.log(value.substring(i + 1).trim());
-                        },
-                        clear: function () {
-                            ngModel.$setViewValue(null);
-                        }
-                    };
-                    // 初始化
-                    _date = laydate.render(_config);
+	.directive('ngLayDate', function ($timeout) {
+		return {
+			require: '?ngModel',
+			restrict: 'A',
+			scope: {
+				ngModel: '='
+			},
+			link: function (scope, element, attr, ngModel) {
+				var _date = null,
+					_config = {};
+				// 渲染模板完成后执行
+				$timeout(function () {
+					// 初始化参数
+					//console.log(ngModel.$modelValue);
+					_config = {
+						elem: element[0],
+						value: ngModel.$modelValue && ngModel.$modelValue.startDate && ngModel.$modelValue.endDate ? ngModel.$modelValue.startDate + ' ' + (attr.range !== undefined && attr.range !== '' ? attr.range : '~') + ' ' + ngModel.$modelValue.endDate : ngModel.$modelValue ? ngModel.$modelValue : '',
+						format: attr.format !== undefined && attr.format !== '' ? attr.format : 'yyyy-MM-dd',
+						range: attr.range !== undefined && attr.range !== '' ? attr.range : false,
+						type: attr.format !== undefined && attr.format !== '' && attr.format === 'yyyy-MM-dd HH:mm:ss' ? 'datetime' : attr.format === 'yyyy' ? 'year' : attr.format === 'yyyy-MM' ? 'month' : attr.format === 'HH:mm:ss' ? 'time' : 'date',
+						//theme: '#5db2ff',
+						//max:attr.hasOwnProperty('maxDate')?attr.maxDate:'',
+						//min:attr.hasOwnProperty('minDate')?attr.minDate:'',
+						done: function (value, date, endDate) {
+							//console.log(value); //得到日期生成的值，如：2017-08-18
+							//console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
+							//console.log(this.range);
+							var i = 0;
+							if (this.range) {
+								i = value.indexOf(this.range);
+								ngModel.$setViewValue({
+									startDate: value.substring(0, i).trim(),
+									endDate: value.substring(i + 1).trim()
+								});
+							} else {
+								ngModel.$setViewValue(value);
+							}
+							//console.log(value.substring(0, i).trim());
+							//console.log(new Date(value.substring(0, i).trim()));
+							//console.log(value.substring(i + 1).trim());
+						},
+						clear: function () {
+							ngModel.$setViewValue(null);
+						}
+					};
 
-                    // 监听日期最大值
-                    if (attr.hasOwnProperty('maxDate')) {
-                        attr.$observe('maxDate', function (val) {
-                            _config.max = val;
-                        })
-                    }
-                    // 监听日期最小值
-                    if (attr.hasOwnProperty('minDate')) {
-                        attr.$observe('minDate', function (val) {
-                            _config.min = val;
-                        })
-                    }
-                }, 0);
-            }
-        };
-    });
+					// 初始化
+					_date = laydate.render(_config);
+
+					// 模型值同步到视图上
+					scope.$watch(function () {
+						return ngModel.$modelValue;
+					}, function (newValue, oldValue) {
+						if (newValue) {
+							if (newValue instanceof String) {
+								element.val(newValue || '');
+							} else if (newValue instanceof Object) {
+								var value = newValue.startDate + ' ' + (attr.range !== undefined && attr.range !== '' ? attr.range : '~') + ' ' + newValue.endDate;
+								element.val(value || '');
+							}
+						}
+					}, true);
+
+
+					// 监听日期最大值
+					if (attr.hasOwnProperty('maxDate')) {
+						attr.$observe('maxDate', function (val) {
+							_config.max = val;
+						})
+					}
+					// 监听日期最小值
+					if (attr.hasOwnProperty('minDate')) {
+						attr.$observe('minDate', function (val) {
+							_config.min = val;
+						})
+					}
+				}, 0);
+			}
+		};
+	});
